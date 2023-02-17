fa5f663ad930e2f8042d5f4a8746c6979b8dab54
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-02-17 17:24:05 -0500

    new font for name on homepage. Name Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return(
     <>
         <header className="firstLastName">
-            <h1>Sophia</h1>
+            <h1 className="first">Sophia</h1>
             <div className="nameTitle">
-                <h1>Lanzendorf</h1>
+                <h1 className="last">Lanzendorf</h1>
                 <h3>Software Engineer</h3>
             </div>
         </header>
