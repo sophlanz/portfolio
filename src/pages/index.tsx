21c8a666351d53ba9886bd80360fbf326688326b
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-02-18 09:36:55 -0500

    added color palette to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return(
     <>
         <header className="firstLastName">
-            <h1 className="first">Sophia</h1>
+            <h1 className="first">S<span>o</span>phia</h1>
             <div className="nameTitle">
-                <h1 className="last">Lanzendorf</h1>
+                <h1 className="last">Lanzend<span>o</span>rf</h1>
                 <h3>Software Engineer</h3>
             </div>
         </header>
