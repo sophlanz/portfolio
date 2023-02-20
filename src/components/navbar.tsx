1a68f0e7cd75d39983270d44aac6c8a70de70383
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-02-20 10:40:29 -0500

    useEffect and set timer to load menu links one by one

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,48 @@
-import React, {useState,createContext} from 'react';
+import React, {useState,useEffect} from 'react';
 import Link from 'next/link';
 import Modal from 'react-modal';
 /* const MenuOpen = createContext<boolean>(false) */
 
 export default function NavBar () {
     const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+    const [displayList, setDisplayList] = useState<JSX.Element[]>([]);
+    const listItems: JSX.Element [] = [  
+    <li> 
+        <Link href='/projects'><span>P</span>R<span>O</span>J<span>E</span>C<span>T</span>S</Link>
+    </li>,
+    <li >
+         <Link href='/about'>A<span>B</span>O<span>U</span>T</Link>
+    </li>,
+    <li >
+          <Link href='/contact'><span>C</span>O<span>N</span>T<span>A</span>C<span>T</span></Link>
+    </li>];
     
     const openModal= () => {
+        //clear old list
+        setDisplayList([])
         setIsOpen(true);
+      
     }
     const closeModal =()=> {
         setIsOpen(false);
+        setDisplayList([])
     }
- 
+    let index: number=0;
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setDisplayList((prevState)=>prevState ? [...prevState,listItems[index]] : [listItems[index]]);
+          index++;
+          if (index >= listItems.length) {
+            clearInterval(interval);
+          }
+        }, 1000);
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [modalIsOpen]);
+console.log(displayList)
+
   return (
     <nav>
         <ul>
@@ -32,15 +62,7 @@ export default function NavBar () {
            >
                 <button className="menuToggle"  onClick={closeModal}>CLOSE</button>
                 <div className="right" style={{display: modalIsOpen===true ? "flex" : "none"}}>
-                      <li>
-                           <Link href='/projects'><span>P</span>R<span>O</span>J<span>E</span>C<span>T</span>S</Link>
-                       </li>
-                       <li>
-                           <Link href='/about'>A<span>B</span>O<span>U</span>T</Link>
-                       </li>
-                       <li>
-                           <Link href='/contact'><span>C</span>O<span>N</span>T<span>A</span>C<span>T</span></Link>
-                       </li>
+                    {displayList}
                 </div>
            </Modal> 
         </ul>
