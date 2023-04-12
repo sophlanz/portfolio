8f7cb9cfb7626b14eb66adf122b48eafe9fc5bad
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-12 14:50:36 -0400

    smooth transition fadeIn fadeOut

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>   
       <LoadingScreen sendToParent={setDisplay}/>
         <header className="firstLastName" style={{display:display}}>
-            <h1 className="first" ><span  data-text="S" style={{fontFamily:"Telegraf-UltraLight"}}>S</span><span style={{fontFamily:"Telegraf-Regular"}}>o</span><span style={{fontFamily:"Telegraf-UltraBold"}}>phia</span></h1>
+            <h1 className="first" ><span data-text="S" style={{fontFamily:"Telegraf-UltraLight"}}>S</span><span style={{fontFamily:"Telegraf-Regular"}}>o</span><span style={{fontFamily:"Telegraf-UltraBold"}}>phia</span></h1>
             <div className="nameTitle">
                 <h1 className="last"><span style={{fontFamily:"Telegraf-UltraBold"}}>Lanzen</span><span style={{fontFamily:"Telegraf-Regular"}}>do</span><span style={{fontFamily:"Telegraf-UltraLight"}}>rf</span></h1>
                 <h3><span style={{fontFamily:"Telegraf-UltraLight"}}>So</span><span style={{fontFamily:"Telegraf-Regular"}}>ftwa</span><span style={{fontFamily:"Telegraf-UltraBold"}}>re Engineer</span></h3>
