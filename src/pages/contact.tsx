2ad0c94184dbe74bd08777d615ad320ed06e839d
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-12 17:22:14 -0400

    fixed vercel single quote error

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
         <>
         <Navbar/>
         <div className="contactContainer">
-            <h1 className="contactGreeting"><span style={{fontFamily:"Telegraf-UltraLight"}}>Do</span><span style={{fontFamily:"Telegraf-Regular"}}>n't Be </span><span style={{fontFamily:"Telegraf-UltraBold"}}>Shy. Say Hi!</span> </h1>
+            <h1 className="contactGreeting"><span style={{fontFamily:"Telegraf-UltraLight"}}>Do</span><span style={{fontFamily:"Telegraf-Regular"}}>n&apos;t Be </span><span style={{fontFamily:"Telegraf-UltraBold"}}>Shy. Say Hi!</span> </h1>
             <form className="contact">
                 <label htmlFor="name">Hello Sophia, I&apos;m
                     <input type="text" name="name" onChange={(e)=> setName(e.target.value)}/>
