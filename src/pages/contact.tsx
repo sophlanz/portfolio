5896c590de1110c10e0a690c90c4df10aab294c8
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-12 17:09:12 -0400

    fixed sigle quote error

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
         <div className="contactContainer">
             <h1 className="contactGreeting"><span style={{fontFamily:"Telegraf-UltraLight"}}>Do</span><span style={{fontFamily:"Telegraf-Regular"}}>n't Be </span><span style={{fontFamily:"Telegraf-UltraBold"}}>Shy. Say Hi!</span> </h1>
             <form className="contact">
-                <label htmlFor="name">Hello Sophia, I'm
+                <label htmlFor="name">Hello Sophia, I&apos;m
                     <input type="text" name="name"/>
                 </label>
                 <label htmlFor="email">Here is my fancy e-mail
