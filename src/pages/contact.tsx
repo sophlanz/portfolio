7e682c4b72a592645f3ab7a1b4be698e16973c72
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-12 17:18:28 -0400

    set state using values from contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,9 @@
 import Navbar from '../components/navbar'
+import { useState } from 'react';
 export default function Contact() {
+    const[name,setName]= useState<string>();
+    const[email,setEmail]=useState<string>();
+    const[message,setMessage]=useState<string>();
     return (
         <>
         <Navbar/>
@@ -7,13 +11,13 @@ export default function Contact() {
             <h1 className="contactGreeting"><span style={{fontFamily:"Telegraf-UltraLight"}}>Do</span><span style={{fontFamily:"Telegraf-Regular"}}>n't Be </span><span style={{fontFamily:"Telegraf-UltraBold"}}>Shy. Say Hi!</span> </h1>
             <form className="contact">
                 <label htmlFor="name">Hello Sophia, I&apos;m
-                    <input type="text" name="name"/>
+                    <input type="text" name="name" onChange={(e)=> setName(e.target.value)}/>
                 </label>
                 <label htmlFor="email">Here is my fancy e-mail
-                    <input type="text" name="email"/>
+                    <input type="text" name="email" onChange={(e)=> setEmail(e.target.value)}/>
                 </label>
                 <label htmlFor="message">I would love to connect about 
-                    <input type="text" name="message"/>
+                    <input type="text" name="message"  onChange={(e)=> setMessage(e.target.value)}/>
                 </label>
                 <button type="submit">Send</button>
             </form>
