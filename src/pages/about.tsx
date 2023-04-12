3855c5641050bdc96ce9eedf7f81a3beaa327ffc
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-12 16:57:01 -0400

    contact page style

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,12 @@
 
 export default function About() {
+    const handleSubmit = () => {
+
+    }
     return (
         <>
         <h1>About</h1>
+        
         </>
     )
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,24 @@
-
+import Navbar from '../components/navbar'
 export default function Contact() {
     return (
         <>
-        <h1>Contact</h1>
+        <Navbar/>
+        <div className="contactContainer">
+            <h1 className="contactGreeting"><span style={{fontFamily:"Telegraf-UltraLight"}}>Do</span><span style={{fontFamily:"Telegraf-Regular"}}>n't Be </span><span style={{fontFamily:"Telegraf-UltraBold"}}>Shy. Say Hi!</span> </h1>
+            <form className="contact">
+                <label htmlFor="name">Hello Sophia, I'm
+                    <input type="text" name="name"/>
+                </label>
+                <label htmlFor="email">Here is my fancy e-mail
+                    <input type="text" name="email"/>
+                </label>
+                <label htmlFor="message">I would love to connect about 
+                    <input type="text" name="message"/>
+                </label>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+       
         </>
     )
 }
