0e0a2d27271e34c8253b018481e6bdb4542fece8
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-13 12:17:11 -0400

    display socials higher up on project screen

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 export default function Socials () {
+    const[top,setTop]=useState<string>('50%');
+    
+    useEffect(()=> {
+        if(typeof window !== "undefined" && window.location.pathname=="/projects") {
+            setTop("25%")
+        }
+    },[])
   return (
-   <div className="socials"> 
+   <div className="socials" style={{top:top}}> 
         <a href='https://github.com/sophlanz' target="_blank" rel="noopener noreferrer">
              <Image src='/svgs/githubSocials.svg' height={20} width={20}alt="github"/>
          </a>
