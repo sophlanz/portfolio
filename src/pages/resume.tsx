bb1ce97536d473727fbd49fd58ec62bcdebbb81b
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-13 20:16:39 -0400

    added resume using react pdf

---
+++ src/pages/resume.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export default function Resume(): JSX.Element {
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div className="resumeContainer" style={{display:"flex", justifyContent:"center"}}>
+      <Document file="/pdfs/Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} scale={1.3} />
+      </Document>
+      {numPages && (
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      )}
+    </div>
+  );
+}
