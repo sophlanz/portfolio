a02285f9536815a291f8ef4dc99d6e16545914d5
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-07 16:04:36 -0400

    timeout for showing loading screen on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,39 +8,42 @@ export default function Home(): JSX.Element {
     const data = window.localStorage.getItem("didInit");
     if (!data) {
       window.localStorage.setItem("didInit", "true");
-    } else {
-      setDidInit(true);
+      setTimeout(() => {
+        return setDidInit(true);
+      }, 3000);
     }
   }, []);
   return (
     <>
-      <Navbar />
       {!didInit ? (
         <LoadingScreen />
       ) : (
-        <header className="firstLastName">
-          <h1 className="first">
-            <span data-text="S" style={{ fontFamily: "Telegraf-UltraLight" }}>
-              S
-            </span>
-            <span style={{ fontFamily: "Telegraf-Regular" }}>o</span>
-            <span style={{ fontFamily: "Telegraf-UltraBold" }}>phia</span>
-          </h1>
-          <div className="nameTitle">
-            <h1 className="last">
-              <span style={{ fontFamily: "Telegraf-UltraBold" }}>Lanzen</span>
-              <span style={{ fontFamily: "Telegraf-Regular" }}>do</span>
-              <span style={{ fontFamily: "Telegraf-UltraLight" }}>rf</span>
-            </h1>
-            <h3>
-              <span style={{ fontFamily: "Telegraf-UltraLight" }}>So</span>
-              <span style={{ fontFamily: "Telegraf-Regular" }}>ftwa</span>
-              <span style={{ fontFamily: "Telegraf-UltraBold" }}>
-                re Engineer
+        <>
+          <Navbar />
+          <header className="firstLastName">
+            <h1 className="first">
+              <span data-text="S" style={{ fontFamily: "Telegraf-UltraLight" }}>
+                S
               </span>
-            </h3>
-          </div>
-        </header>
+              <span style={{ fontFamily: "Telegraf-Regular" }}>o</span>
+              <span style={{ fontFamily: "Telegraf-UltraBold" }}>phia</span>
+            </h1>
+            <div className="nameTitle">
+              <h1 className="last">
+                <span style={{ fontFamily: "Telegraf-UltraBold" }}>Lanzen</span>
+                <span style={{ fontFamily: "Telegraf-Regular" }}>do</span>
+                <span style={{ fontFamily: "Telegraf-UltraLight" }}>rf</span>
+              </h1>
+              <h3>
+                <span style={{ fontFamily: "Telegraf-UltraLight" }}>So</span>
+                <span style={{ fontFamily: "Telegraf-Regular" }}>ftwa</span>
+                <span style={{ fontFamily: "Telegraf-UltraBold" }}>
+                  re Engineer
+                </span>
+              </h3>
+            </div>
+          </header>
+        </>
       )}
     </>
   );
