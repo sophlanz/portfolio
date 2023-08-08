22d590b2ad5ea1bcea84e3ba705115a93f371bed
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-08 10:08:17 -0400

    Contact form compound component

--- src/components/contact/contactForm.tsx
+++ src/components/contact/contactForm.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormLabel from "./formLabel";
 interface Props {
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   setName: React.Dispatch<React.SetStateAction<string>>;
@@ -7,9 +8,29 @@ interface Props {
   statusMessage: string;
 }
 export default function ContactForm(props: Props): JSX.Element {
+  const { handleSubmit, setName, setEmail, setMessage, statusMessage } = props;
   return (
-    <form className="contact" onSubmit={(e) => props.handleSubmit(e)}>
-      {/*   */}
+    <form className="contact" onSubmit={(e) => handleSubmit(e)}>
+      <FormLabel
+        htmlFor="name"
+        type="text"
+        name="name"
+        onChangeSetState={setName}
+      />
+      <FormLabel
+        htmlFor="email"
+        type="text"
+        name="email"
+        onChangeSetState={setEmail}
+      />
+      <FormLabel
+        htmlFor="message"
+        type="text"
+        name="message"
+        onChangeSetState={setMessage}
+      />
+      <p>{statusMessage}</p>
+      <button type="submit">Send</button>
     </form>
   );
 }
