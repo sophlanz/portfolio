2ba4872f516cd09bb45b8c26098a84ca70af6d70
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-08 09:42:25 -0400

    compound component for project header

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useRef, useEffect } from "react";
 import Navbar from "../components/nav";
 import AllProjects from "@/components/projects";
+import ProjectHeader from "@/components/projects/projectHeader";
 export default function Projects(): JSX.Element {
   const projectsContainerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -22,11 +23,7 @@ export default function Projects(): JSX.Element {
       <Navbar />
       <div className="projects">
         <div className="projectsContainer" ref={projectsContainerRef}>
-          <h1 className="project">
-            <span style={{ fontFamily: "Telegraf-UltraLight" }}>P</span>
-            <span style={{ fontFamily: "Telegraf-Regular" }}>RO</span>
-            <span style={{ fontFamily: "Telegraf-UltraBold" }}>JECTS</span>
-          </h1>
+          <ProjectHeader />
           <AllProjects />
         </div>
       </div>
