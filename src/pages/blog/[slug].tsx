8571381df1d24db2a52432ec9b8f44316d076434
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-09 19:22:30 -0400

    get slug paths for specific blog page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,7 +2,7 @@ import React from "react";
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
 import glob from "glob";
-import { GetStaticProps, GetStaticPropsContext, GetStaticPaths } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { ParsedUrlQuery } from "querystring";
 interface contextParams extends ParsedUrlQuery {
   slug: string;
@@ -21,5 +21,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
       frontmatter: data,
       markdownContent: content,
     },
+  };
+};
+export const getStaticPaths: GetStaticPaths = async () => {
+  //get all markdown (.md) files in the posts dir
+  const blogs = glob.sync("posts/**/*.md");
+
+  //remove path and extension to leave filename only
+  const blogSlugs = blogs.map((file) =>
+    file.split("/")[1].replace(/ /g, "-").slice(0, -3).trim()
+  );
+
+  // create paths with `slug` param
+  const paths = blogSlugs.map((slug) => {
+    return { params: { slug: slug } };
+  });
+  return {
+    paths,
+    fallback: false,
   };
 };
